667efa8a13a273333a6480b7d5c7b23b9ad7881b
Hang DoThiDuc <Hang.DoThiDuc@rga.com>
2016-07-08 16:36:15 -0400

    static scraping test

---
+++ xray.js
@@ -0,0 +1,36 @@
+var Xray = require('x-ray');
+var x = Xray();
+var fs = require('fs');
+
+x('http://www.google.com/trends/fetchComponent?hl=en-US&q=juno%20probe&geo=JP&date=now%207-d&cmpt=q&tz=Etc/GMT%2B4&tz=Etc/GMT%2B4&content=1&cid=GEO_MAP_0_0&export=0', 'tr', [{
+  name: 'a',
+  code: 'a@href',
+  value: '.trends-hbars-value'
+}])(function(err, data){
+    console.log(data);
+    prepareData(data);
+})
+
+function prepareData(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var key in arr[i]) {
+            if (key ==  "value"){
+                arr[i][key] = arr[i][key].replace(/[^0-9]+/g, "");
+            } else if (key == "name") {
+                arr[i][key] = arr[i][key].replace(/ *\([^)]*\) */g, "");
+                arr[i][key] = arr[i][key].replace("\n", "");
+            } else {
+                var str = arr[i][key];
+                arr[i][key] = str.substring(str.lastIndexOf("geo=")+4,str.lastIndexOf("&date"));
+            }
+            console.log(i, arr[i][key]);
+        }
+    }
+    saveData(arr);
+}
+
+function saveData(_data) {
+    fs.writeFile("data/" + Date.now() + ".json", JSON.stringify(_data), function() {
+        console.log("data saved.");
+    });
+}
